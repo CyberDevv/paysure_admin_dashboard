578910cfa64ee8e41517f87606e32a5b5cf6ba6b
Cyberdevv <tocyberdevv@gmail.com>
2022-01-24 01:32:06 +0100

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+// imports
+import React from 'react'
+import tw from 'twin.macro'
+
+// Page init
+const IndexPage = () => <P>Paysure Admin Dashboard</P>
+
+// Tailwind Styles
+const P = tw.div`text-2xl`
+
+// Page export
+export default IndexPage
