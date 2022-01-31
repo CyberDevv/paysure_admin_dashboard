a5fb71387243c4f51929080e423a72770abecc72
Cyberdevv <tocyberdevv@gmail.com>
2022-01-31 16:56:56 +0100

    💥 NEW: Make the add permission functionality

---
+++ components/CreatePermissionDashboard.jsx
@@ -0,0 +1,203 @@
+import React from 'react'
+import tw from 'twin.macro'
+import PropTypes from 'prop-types'
+import { Checkbox } from '@mui/material'
+import { styled } from '@mui/material/styles'
+
+const CreatePermissionDashboard = ({ setShowAddPanel }) => {
+  // functions
+  const handleGoBack = React.useCallback(() => {
+    setShowAddPanel(false)
+  })
+
+  return (
+    <div>
+      <Button onClick={handleGoBack}>Back</Button>
+
+      <Ttile>Create Permissions</Ttile>
+
+      {/* fields */}
+      <Form>
+        <Label>
+          Title
+          <Input type="text" placeholder="Adminstrator" />
+        </Label>
+
+        <div css={[tw`flex justify-between items-center w-full space-x-4`]}>
+          <CheckboxWrapper>
+            <Checkbox color="error" />
+          
+            <P>Allow all permissions</P>
+          </CheckboxWrapper>
+          
+          {/* Save button */}
+          <MUIButton>Save</MUIButton>
+        </div>
+      </Form>
+
+      {/* Grid */}
+      <GridWrapper>
+        {/* header */}
+        <GridHeader>
+          <HeaderText>Feature</HeaderText>
+          <HeaderTextOther>View</HeaderTextOther>
+          <HeaderTextOther>Update</HeaderTextOther>
+          <HeaderTextOther>Delete</HeaderTextOther>
+          <HeaderTextOther>Create</HeaderTextOther>
+          <HeaderTextOther>List</HeaderTextOther>
+          <HeaderTextOther>Print</HeaderTextOther>
+          <HeaderTextOther>Add</HeaderTextOther>
+          <HeaderTextOther>Attach</HeaderTextOther>
+        </GridHeader>
+
+        {/* body */}
+        <div>
+          {/* Transaction */}
+          <Grid>
+            <HeaderText>Transactions</HeaderText>
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+          </Grid>
+
+          {/* Providers */}
+          <Grid>
+            <HeaderText>Providers</HeaderText>
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+          </Grid>
+
+          {/* Organizations */}
+          <Grid>
+            <HeaderText>Organizations</HeaderText>
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+          </Grid>
+
+          {/* Users */}
+          <Grid>
+            <HeaderText>Users</HeaderText>
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+          </Grid>
+
+          {/* Super Agents */}
+          <Grid>
+            <HeaderText>Super Agents</HeaderText>
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+          </Grid>
+
+          {/* Agents */}
+          <Grid>
+            <HeaderText>Agents</HeaderText>
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+          </Grid>
+
+          {/* Roles */}
+          <Grid>
+            <HeaderText>Roles</HeaderText>
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+          </Grid>
+
+          {/* Settlements */}
+          <Grid>
+            <HeaderText>Settlements</HeaderText>
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+          </Grid>
+
+          {/* VAS */}
+          <Grid>
+            <HeaderText>VAS</HeaderText>
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+            <MUICheckbox />
+          </Grid>
+        </div>
+      </GridWrapper>
+    </div>
+  )
+}
+
+const MUICheckbox = styled(Checkbox)`
+  ${tw`rounded-none`}
+`
+
+// Tailwind Styles
+const Button = tw.button`normal-case text-paysure-text-100 text-sm hover:(underline)`
+const Ttile = tw.h1`text-gray-dark tracking-[-0.025em] text-lg mt-4 lg:(text-[20px] mt-8)`
+const GridWrapper = tw.div`mt-10`
+const Grid = tw.div`grid grid-template-columns[2.5fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr] p-4 gap-4`
+const GridHeader = tw(Grid)`bg-blue-light`
+const HeaderText = tw.p`text-[13px] my-auto`
+const HeaderTextOther = tw(HeaderText)`mx-auto`
+const Form = tw.form`mt-11 flex items-end`
+const Label = tw.label`text-[13px] text-[#454D54]`
+const Input = tw.input`text-[13px] block border border-border mt-2 px-3.5 py-3 w-full rounded w-[336px] focus:(outline-none ring-2 ring-border2)`
+const CheckboxWrapper = tw.div`flex items-center bg-[#FCEAE8] p-[3px] rounded ml-2.5 w-[254px]`
+const P = tw.p`text-[13px] text-[#ED6A5A]`
+const MUIButton = tw(
+  Button,
+)`bg-paysure-100 text-white normal-case rounded-lg p-3 px-10 text-[13px] hover:(bg-paysure-100 ring-2 ring-offset-2 ring-paysure-100)`
+
+// proptypes
+CreatePermissionDashboard.propTypes = {
+  setShowAddPanel: PropTypes.func.isRequired,
+}
+
+export default CreatePermissionDashboard
--- components/PermissionsSettings.jsx
+++ components/PermissionsSettings.jsx
@@ -1,151 +1,22 @@
-import tw from 'twin.macro'
 import React from 'react'
-import { Button } from '@mui/material'
 
-import { Add } from './SVGIcons'
-import { DataGridViewTemp } from '.'
+import { PermissionsSettingsDashboard, CreatePermissionDashboard } from '.'
 
 const PermissionsSettings = () => {
+  // useState hooks
+  const [showAddPanel, setShowAddPanel] = React.useState(true)
+
   return (
     <>
-      <div css={[tw`flex justify-between items-center w-full`]}>
-        <div>
-          <Ttile className="font-500">Permissions</Ttile>
-          <TitleSpan>Create a settlement band</TitleSpan>
-        </div>
-        <MUIButton startIcon={<Add />}>Add Permissions</MUIButton>
-      </div>
+      {!showAddPanel && (
+        <PermissionsSettingsDashboard setShowAddPanel={setShowAddPanel} />
+      )}
 
-      <DataGridViewTemp
-        rows={rows}
-        columns={columns}
-        dropdownData={dropdownData}
-      />
+      {showAddPanel && (
+        <CreatePermissionDashboard setShowAddPanel={setShowAddPanel} />
+      )}
     </>
   )
 }
-
-// FIXME: Temp data (should be replaced with real data)
-const dropdownData = [
-  {
-    value: 'all',
-    label: 'All',
-  },
-  {
-    value: 'user',
-    label: 'User',
-  },
-  {
-    value: 'admin',
-    label: 'Admin',
-  },
-]
-
-// FIXME: Temp data (should be replaced with real data)
-const rows = [
-  {
-    id: 1,
-    col1: 1,
-    col2: 'Apple',
-    col3: 'POS',
-    col4: 1,
-    col5: 4243,
-    col6: '443943043',
-    col7: '443943043',
-    col8: '7013',
-    col9: 'Dec 30, 2018 05:12',
-    col10: '',
-  },
-  {
-    id: 2,
-    col1: 2,
-    col2: 'Master Card',
-    col3: 'POS',
-    col4: 1,
-    col5: 4243,
-    col6: '443943043',
-    col7: '443943043',
-    col8: '7013',
-    col9: 'Dec 30, 2018 05:12',
-    col10: '',
-  },
-  {
-    id: 3,
-    col1: 3,
-    col2: 'Bessie Cooper',
-    col3: 'Tv Subscription',
-    col4: 5000,
-    col5: 39.9,
-    col6: '443943043',
-    col7: 'Bank Card',
-    col8: 'pending',
-    col9: 'Dec 30, 2018 05:12',
-    col10: '',
-  },
-  {
-    id: 4,
-    col1: 4,
-    col2: 'Bessie Cooper',
-    col3: 'Tv Subscription',
-    col4: 5000,
-    col5: 39.9,
-    col6: '443943043',
-    col7: 'Bank Card',
-    col8: 'completed',
-    col9: 'Dec 30, 2018 05:12',
-    col10: '',
-  },
-  {
-    id: 5,
-    col1: 5,
-    col2: 'Bessie Cooper',
-    col3: 'Tv Subscription',
-    col4: 5000,
-    col5: 39.9,
-    col6: '443943043',
-    col7: 'Bank Card',
-    col8: 'pending',
-    col9: 'Dec 30, 2018 05:12',
-    col10: '',
-  },
-]
-
-// FIXME: Temp data (should be replaced with real data)
-const columns = [
-  {
-    field: 'col1',
-    headerName: 'S/N',
-    minWidth: 71,
-    flex: 1,
-    headerClassName: 'grid-header',
-  },
-  {
-    field: 'col2',
-    headerName: 'Role',
-    minWidth: 227,
-    flex: 1,
-    headerClassName: 'grid-header',
-  },
-  {
-    field: 'col3',
-    headerName: 'Permissions',
-    minWidth: 236,
-    flex: 1,
-    headerClassName: 'grid-header',
-  },
-  {
-    field: 'col4',
-    headerName: 'Action.',
-    minWidth: 100,
-    flex: 1,
-    headerClassName: 'grid-header',
-  },
-]
-
-const Ttile = tw.h1`text-gray-dark tracking-[-0.05em] text-2xl lg:(text-[20px])`
-const TitleSpan = tw.span`block text-light-dark text-sm font-normal mt-1.5 tracking-normal lg:(mt-3 text-base)`
-const MUIButton = tw(
-  Button,
-)`bg-paysure-100 text-white normal-case rounded-lg p-3 pl-3.5 text-[13px] hover:(bg-paysure-100 ring-2 ring-offset-2 ring-paysure-100)`
 
 export default PermissionsSettings
