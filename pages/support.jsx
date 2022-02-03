1007b8f2d173e00967d69a3335296c29d804d984
Cyberdevv <tocyberdevv@gmail.com>
2022-02-03 17:08:28 +0100

    💥 NEW: create the support page

---
+++ pages/support.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { SupportDashboard } from '../components'
+
+const Support = () => {
+  return (
+    <>
+      <Head>
+        <title>Support | Paysure</title>
+      </Head>
+
+      <SupportDashboard />
+    </>
+  )
+}
+
+export default Support
