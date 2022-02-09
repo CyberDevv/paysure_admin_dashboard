4eb793cce763fa68288fd9e4465b8e539c6020fd
Cyberdevv <tocyberdevv@gmail.com>
2022-02-09 05:26:46 +0100

    👌 IMPROVE: make the settings page responsive on all devices

--- components/page_components/SettingsDashboard.jsx
+++ components/page_components/SettingsDashboard.jsx
@@ -1,6 +1,6 @@
 import tw from 'twin.macro'
 import React, { useState } from 'react'
-import { Button, Divider } from '@mui/material'
+import { Button, Divider, SpeedDial, SpeedDialAction } from '@mui/material'
 
 import Layout from '../layouts/main_layout/index.main_layout'
 import {
@@ -9,12 +9,18 @@ import {
 } from '../SVGIcons'
 import { BandsSettings, PermissionsSettings, Plan_PackagesSettings } from '..'
 
+const actions = [
+  { icon: <CategoriesSettingsSVG />, name: 'Bands' },
+  { icon: <PermissionsSettingsSVG />, name: 'Permissions' },
+  { icon: <CategoriesSettingsSVG />, name: 'Plan Packages' },
+]
+
 const SettingsDashboard = () => {
   // UseState Hooks
   const [activeTab, setActiveTab] = useState('Bands')
 
   // Functions
-  const handleSetActiveTab = React.useCallback((label) => {
+  const handleSetActiveTab = React.useCallback(label => {
     setActiveTab(label)
   })
 
@@ -22,9 +28,12 @@ const SettingsDashboard = () => {
   const NavItem = ({ label, icon }) => {
     return (
       <li>
-        <MUIButton sx={[
-          activeTab === label ? tw`text-paysure-100` : tw`text-gray-700`,
-        ]} fullWidth startIcon={icon} onClick={() => handleSetActiveTab(label)}>
+        <MUIButton
+          sx={[activeTab === label ? tw`text-paysure-100` : tw`text-gray-700`]}
+          fullWidth
+          startIcon={icon}
+          onClick={() => handleSetActiveTab(label)}
+        >
           <Span>{label}</Span>
         </MUIButton>
       </li>
@@ -32,29 +41,51 @@ const SettingsDashboard = () => {
   }
 
   return (
-    <Layout title="Settins">
-      <SectionsWrapper>
-        {/* Section1 */}
-        <div css={[tw`mt-8`]}>
-          <Ttile className="font-bold">Settings</Ttile>
+    <>
+      <Layout title="Settins">
+        <SectionsWrapper>
+          {/* Section1 */}
+          <div css={[tw`mt-8 hidden xl:block`]}>
+            <Ttile className="font-bold">Settings</Ttile>
 
-          <Nav>
-            <NavItem label="Bands" icon={<CategoriesSettingsSVG />} />
-            <NavItem label="Permissions" icon={<PermissionsSettingsSVG />} />
-            <NavItem label="Plan Packages" icon={<CategoriesSettingsSVG />} />
-          </Nav>
-        </div>
+            <Nav>
+              <NavItem label="Bands" icon={<CategoriesSettingsSVG />} />
+              <NavItem label="Permissions" icon={<PermissionsSettingsSVG />} />
+              <NavItem label="Plan Packages" icon={<CategoriesSettingsSVG />} />
+            </Nav>
+          </div>
 
-        <Divider css={[tw`ml-[125px] mr-10`]} orientation="vertical" flexItem />
+          <Divider
+            css={[tw`ml-[85px] mr-10 hidden xl:block 2xl:ml-[125px]`]}
+            orientation="vertical"
+            flexItem
+          />
 
-        {/* sections2 */}
-        <div css={[tw`w-full mt-8`]}>
-          {activeTab === 'Bands' && <BandsSettings />}
-          {activeTab === 'Permissions' && <PermissionsSettings />}
-          {activeTab === 'Plan Packages' && <Plan_PackagesSettings />}
-        </div>
-      </SectionsWrapper>
-    </Layout>
+          {/* sections2 */}
+          <div css={[tw`w-full xl:mt-8`]}>
+            {activeTab === 'Bands' && <BandsSettings />}
+            {activeTab === 'Permissions' && <PermissionsSettings />}
+            {activeTab === 'Plan Packages' && <Plan_PackagesSettings />}
+          </div>
+        </SectionsWrapper>
+      </Layout>
+
+      <SpeedDial
+        ariaLabel="SpeedDial"
+        sx={tw`fixed bottom-4 right-4 xl:hidden`}
+        icon={<PermissionsSettingsSVG />}
+      >
+        {actions.map(action => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            tooltipOpen
+            onClick={() => handleSetActiveTab(action.name)}
+          />
+        ))}
+      </SpeedDial>
+    </>
   )
 }
 
