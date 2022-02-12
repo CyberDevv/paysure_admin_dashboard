71b01969059c3226753fd14d78299a30d4ee4b31
Cyberdevv <tocyberdevv@gmail.com>
2022-02-12 17:59:45 +0100

    👌 IMPROVE: make the pages linked to settings page responsive

--- components/CreatePermissionDashboard.jsx
+++ components/CreatePermissionDashboard.jsx
@@ -36,140 +36,142 @@ const CreatePermissionDashboard = ({ setShowAddPanel }) => {
       </Form>
 
       {/* Grid */}
-      <GridWrapper>
-        {/* header */}
-        <GridHeader>
-          <HeaderText>Feature</HeaderText>
-          <HeaderTextOther>View</HeaderTextOther>
-          <HeaderTextOther>Update</HeaderTextOther>
-          <HeaderTextOther>Delete</HeaderTextOther>
-          <HeaderTextOther>Create</HeaderTextOther>
-          <HeaderTextOther>List</HeaderTextOther>
-          <HeaderTextOther>Print</HeaderTextOther>
-          <HeaderTextOther>Add</HeaderTextOther>
-          <HeaderTextOther>Attach</HeaderTextOther>
-        </GridHeader>
-
-        {/* body */}
-        <div>
-          {/* Transaction */}
-          <Grid>
-            <HeaderText>Transactions</HeaderText>
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-          </Grid>
-
-          {/* Providers */}
-          <Grid>
-            <HeaderText>Providers</HeaderText>
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-          </Grid>
-
-          {/* Organizations */}
-          <Grid>
-            <HeaderText>Organizations</HeaderText>
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-          </Grid>
-
-          {/* Users */}
-          <Grid>
-            <HeaderText>Users</HeaderText>
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-          </Grid>
-
-          {/* Super Agents */}
-          <Grid>
-            <HeaderText>Super Agents</HeaderText>
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-          </Grid>
-
-          {/* Agents */}
-          <Grid>
-            <HeaderText>Agents</HeaderText>
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-          </Grid>
-
-          {/* Roles */}
-          <Grid>
-            <HeaderText>Roles</HeaderText>
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-          </Grid>
-
-          {/* Settlements */}
-          <Grid>
-            <HeaderText>Settlements</HeaderText>
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-          </Grid>
-
-          {/* VAS */}
-          <Grid>
-            <HeaderText>VAS</HeaderText>
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-            <MUICheckbox />
-          </Grid>
-        </div>
-      </GridWrapper>
+      <div tw= "overflow-x-scroll">
+        <GridWrapper>
+          {/* header */}
+          <GridHeader>
+            <HeaderText>Feature</HeaderText>
+            <HeaderTextOther>View</HeaderTextOther>
+            <HeaderTextOther>Update</HeaderTextOther>
+            <HeaderTextOther>Delete</HeaderTextOther>
+            <HeaderTextOther>Create</HeaderTextOther>
+            <HeaderTextOther>List</HeaderTextOther>
+            <HeaderTextOther>Print</HeaderTextOther>
+            <HeaderTextOther>Add</HeaderTextOther>
+            <HeaderTextOther>Attach</HeaderTextOther>
+          </GridHeader>
+        
+          {/* body */}
+          <div>
+            {/* Transaction */}
+            <Grid>
+              <HeaderText>Transactions</HeaderText>
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+            </Grid>
+        
+            {/* Providers */}
+            <Grid>
+              <HeaderText>Providers</HeaderText>
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+            </Grid>
+        
+            {/* Organizations */}
+            <Grid>
+              <HeaderText>Organizations</HeaderText>
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+            </Grid>
+        
+            {/* Users */}
+            <Grid>
+              <HeaderText>Users</HeaderText>
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+            </Grid>
+        
+            {/* Super Agents */}
+            <Grid>
+              <HeaderText>Super Agents</HeaderText>
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+            </Grid>
+        
+            {/* Agents */}
+            <Grid>
+              <HeaderText>Agents</HeaderText>
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+            </Grid>
+        
+            {/* Roles */}
+            <Grid>
+              <HeaderText>Roles</HeaderText>
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+            </Grid>
+        
+            {/* Settlements */}
+            <Grid>
+              <HeaderText>Settlements</HeaderText>
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+            </Grid>
+        
+            {/* VAS */}
+            <Grid>
+              <HeaderText>VAS</HeaderText>
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+              <MUICheckbox />
+            </Grid>
+          </div>
+        </GridWrapper>
+      </div>
     </div>
   )
 }
@@ -181,15 +183,15 @@ const MUICheckbox = styled(Checkbox)`
 // Tailwind Styles
 const Button = tw.button`normal-case text-paysure-text-100 text-sm hover:(underline)`
 const Ttile = tw.h1`text-gray-dark tracking-[-0.025em] text-lg mt-4 lg:(text-[20px] mt-8)`
-const GridWrapper = tw.div`mt-10`
+const GridWrapper = tw.div`mt-10 min-w-[720px]`
 const Grid = tw.div`grid grid-template-columns[2.5fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr] p-4 gap-4`
 const GridHeader = tw(Grid)`bg-blue-light`
 const HeaderText = tw.p`text-[13px] my-auto`
 const HeaderTextOther = tw(HeaderText)`mx-auto`
-const Form = tw.form`mt-11 flex items-end`
-const Label = tw.label`text-[13px] text-[#454D54]`
-const Input = tw.input`text-[13px] block border border-border mt-2 px-3.5 py-3 w-full rounded w-[336px] focus:(outline-none ring-2 ring-border2)`
-const CheckboxWrapper = tw.div`flex items-center bg-[#FCEAE8] p-[3px] rounded ml-2.5 w-[254px]`
+const Form = tw.form`mt-11 flex flex-col space-y-2 lg:(flex-row space-y-0 items-end)`
+const Label = tw.label`text-[13px] text-[#454D54] w-full lg:w-[inherit]`
+const Input = tw.input`text-[13px] block border border-border mt-2 px-3.5 py-3 w-full rounded lg:w-[336px] focus:(outline-none ring-2 ring-border2)`
+const CheckboxWrapper = tw.div`flex items-center bg-[#FCEAE8] p-[3px] rounded lg:ml-2.5 w-[254px]`
 const P = tw.p`text-[13px] text-[#ED6A5A]`
 const MUIButton = tw(
   Button,
