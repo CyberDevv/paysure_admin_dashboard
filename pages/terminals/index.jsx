85a29f944311a7774107b20a891d7356dd87b3ed
Cyberdevv <tocyberdevv@gmail.com>
2022-03-01 16:02:35 +0100

    💡 Commenting: add code to fetch list of terminals and commented out

--- pages/terminals/index.jsx
+++ pages/terminals/index.jsx
@@ -1,8 +1,10 @@
 // imports
 import React from 'react'
 import Head from 'next/head'
+// import nookies from 'nookies'
 
 import { TerminalsDashboard } from '../../components'
+// import { makeEncryptedRequest } from '../../utils/makeEncryptedRequest'
 
 // Page init
 const Terminals = () => {
@@ -16,6 +18,26 @@ const Terminals = () => {
     </>
   )
 }
+
+// getStaticProps
+// export async function getServerSideProps(ctx) {
+//   const { USER_AUTHORIZATION } = nookies.get(ctx)
+
+//   const response = await makeEncryptedRequest(
+//     { status: 0, fromDate: '2021-01-01 00:00:00', page: '2022-03-31 23:59:59' },
+//     '3geepay/api/processor/list-terminal-info',
+//     'POST',
+//     USER_AUTHORIZATION,
+//   )
+
+//   console.log(response)
+
+//   return {
+//     props: {
+//       // data: response ? response.data : {},
+//     },
+//   }
+// }
 
 // Page export
 export default Terminals
