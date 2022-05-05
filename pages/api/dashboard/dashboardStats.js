69c8ba85b3dd4e4234400bd1a2ba20525f075774
Cyberdevv <tocyberdevv@gmail.com>
2022-05-05 11:18:07 +0100

    🐛 FIX: remove dashboardStats.js and listTerminal.js

--- pages/api/dashboard/dashboardStats.js
+++ pages/api/dashboard/dashboardStats.js
@@ -1,14 +1,14 @@
-import { makeEmptyRequest } from '../../../utils/makeEmptyRequest'
+// import { makeEmptyRequest } from '../../../utils/makeEmptyRequest'
 
-export default async function dashboardStats(req, res) {
-  try {
-    const response = await makeEmptyRequest(
-      'paysure/api/processor/dashboard-stats',
-      'POST',
-    )
+// export default async function dashboardStats(req, res) {
+//   try {
+//     const response = await makeEmptyRequest(
+//       'paysure/api/processor/dashboard-stats',
+//       'POST',
+//     )
 
-    res.status(response.status).json(response)
-  } catch (error) {
-    console.log(error.response.data)
-  }
-}
+//     res.status(response.status).json(response)
+//   } catch (error) {
+//     console.log(error.response.data)
+//   }
+// }
--- pages/api/terminals/listTerminals.js
+++ pages/api/terminals/listTerminals.js
@@ -1,17 +1,17 @@
-import { makePlainRequest } from '../../../utils/makePlainReques'
+// import { makePlainRequest } from '../../../utils/makePlainReques'
 
-export default async function ListActiveRoles(req, res) {
-  try {
-    const { status, fromDate, toDate } = req.body
+// export default async function ListActiveRoles(req, res) {
+//   try {
+//     const { status, fromDate, toDate } = req.body
 
-    const response = await makePlainRequest(
-      { status, fromDate, toDate },
-      'paysure/api/processor/list-terminal-info',
-      'POST',
-    )
+//     const response = await makePlainRequest(
+//       { status, fromDate, toDate },
+//       'paysure/api/processor/list-terminal-info',
+//       'POST',
+//     )
 
-    res.status(response.status).json(response)
-  } catch (error) {
-    console.log(error.response.data)
-  }
-}
+//     res.status(response.status).json(response)
+//   } catch (error) {
+//     console.log(error.response.data)
+//   }
+// }
