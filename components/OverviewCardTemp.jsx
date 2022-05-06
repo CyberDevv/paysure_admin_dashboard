c52521b54d552a2679f0bf684cbdd16122160cc7
Cyberdevv <tocyberdevv@gmail.com>
2022-05-06 12:04:22 +0100

    🐛 FIX: fix issue of overviewCard styles not showing

--- components/OverviewCardTemp.jsx
+++ components/OverviewCardTemp.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import tw from 'twin.macro'
-import Proptypes from 'prop-types'
 import Link from 'next/link'
+import Proptypes from 'prop-types'
 
+// Tailwind Styles
+const Wrapper = tw.div`border border-border rounded-lg p-4 min-w-[130px] lg:(py-6 px-8 min-w-[235px])`
+const H1 = tw.h1`text-dark text-xl sm:text-2xl lg:text-[28px] xl:(text-[32px])`
+const P = tw.p`text-paysure-50 text-sm mt-1.5 lg:(text-base mt-3)`
 const OverviewCardTemp = ({ amount, label, onClick }) => {
   return (
     <>
@@ -32,10 +36,5 @@ OverviewCardTemp.prototype = {
   amount: Proptypes.number.isRequired,
   label: Proptypes.string.isRequired,
 }
-
-// Tailwind Styles
-const Wrapper = tw.a`border border-border rounded-lg p-4 min-w-[130px] lg:(py-6 px-8 min-w-[235px])`
-const H1 = tw.h1`text-dark text-xl sm:text-2xl lg:text-[28px] xl:(text-[32px])`
-const P = tw.p`text-paysure-50 text-sm mt-1.5 lg:(text-base mt-3)`
 
 export default OverviewCardTemp
