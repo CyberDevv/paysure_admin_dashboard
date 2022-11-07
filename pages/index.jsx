22075b0716f371678e0b07aed7ae3a898d21d522
cyberdevv <tocyberdevv@gmail.com>
2022-11-07 14:08:55 +0100

    ...

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,43 +1,15 @@
 import Head from 'next/head'
-import nookies from 'nookies'
 import React from 'react'
 
-import { HomeDashboard } from '../components'
-import { fetcher } from '../utils/fetcher'
-
-// export async function getServerSideProps(ctx) {
-//   const { USER_TOKEN } = nookies.get(ctx)
-
-//   const response = await fetcher(
-//     USER_TOKEN,
-//     'GET',
-//     '/apis/v1/paysure/admin/adminMainPage/analytics',
-//   )
-
-//   if (response.status === 401) {
-//     return {
-//       redirect: {
-//         destination: '/login',
-//         permanent: false,
-//       },
-//     }
-//   }
-
-//   return {
-//     props: {
-//       data: response.data,
-//     },
-//   }
-// }
-
-export default function Home({ data }) {
+export default function Home() {
   return (
     <>
       <Head>
         <title>Home | Paysure</title>
       </Head>
 
-      <HomeDashboard homePageStats={data} />
+      {/* <HomeDashboard homePageStats={data} /> */}
+      <p>What the fuck is wrong with you</p>
     </>
   )
 }
